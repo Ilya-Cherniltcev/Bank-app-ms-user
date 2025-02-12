94a249dea90f5b297ffc3ad116d6bb6c40cba7c0
Ilya Cherniltcev <cherniltcev@mail.ru>
2025-02-13 00:19:05 +0500

    init commit

---
+++ src/main/java/com/org/bankmsuser/BankMsUserApplication.java
@@ -0,0 +1,13 @@
+package com.org.bankmsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMsUserApplication.class, args);
+	}
+
+}
