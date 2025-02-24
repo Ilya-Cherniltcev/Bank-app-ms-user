51a5c38ddefacbd9e05f87109b79b4b695bf3ba0
mr-arzh <asiris.joy@icloud.com>
2025-02-24 22:36:11 +0300

    feat: created UserController and function getUser

---
+++ src/main/java/com/org/bankmsuser/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.org.bankmsuser.controller;
+
+import com.org.bankmsuser.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id){
+            return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+
+
+}
